60229820e2615dc02f8e068584b8ab983b0849e0
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-02-15 19:33:17 -0600

    Add files via upload

---
+++ ejercicio2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(){
+    int var1 = 10;
+    int var2 = 20;
+    int var3 = 2;
+    int res = var1 * var2 * var3;
+
+    printf("El ressultado de multiplicar %d * %d * %d es: %d", var1, var2, var3, res);
+    printf("\n");
+
+    res= var1 * var2 * var3;
+    printf("El resultado es: %d", res);
+
+    printf("\n");
+    system("Pause");
+    return 0;
+}
