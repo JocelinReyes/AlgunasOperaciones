d7a791f19c8112da843d2d8e111f095765dc58ce
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-02-15 19:49:06 -0600

    operacionesBasicas.c

--- ejemloDouble.c
+++ ejemloDouble.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main (){
+    
+    //primero empezaremos con la mas basica que es la suma
+    
+    //declaramos las variables que vamos a utilizar
+    int unNumero = 3;
+    int otroNumero = 4;
+    
+    //quiero imprimir la suma de 3 mas 4
+    printf("%d", unNumero + otroNumero);
+
     double r = 5;
     double pi = 3.1416;
     double area = pi * r * r;
