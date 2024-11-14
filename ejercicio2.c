06ca7349fe463043f13134326b74a987dc7f651b
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-11-13 20:01:11 -0600

    Actualizar ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -6,7 +6,7 @@ int main(){
     int var3 = 2;
     int res = var1 * var2 * var3;
 
-    printf("El ressultado de multiplicar %d * %d * %d es: %d", var1, var2, var3, res);
+    printf("El ressultado de multiplicar %d * %d * %d es: %d", var1, var2, var3, res); //resultado 
     printf("\n");
 
     res= var1 * var2 * var3;
