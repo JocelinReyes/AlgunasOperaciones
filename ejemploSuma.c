d16728233db41b1e2b863b6e9a316257065865c5
JocelinReyes <160088658+JocelinReyes@users.noreply.github.com>
2024-11-17 21:55:25 -0600

    Actualizar ejemploSuma.c

--- ejemploSuma.c
+++ ejemploSuma.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int unNumero = 3; // número a sumar
+    int unNumero = 3; // número un sumar
     int otroNumero = 4;
     
     //quiero imprimir la suma de 3 mas 4
